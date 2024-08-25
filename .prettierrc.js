597b3d439cf864f317b9e60964ad0fb40b4c7781
Alex Fomushkin <aleksefo@gmail.com>
2024-08-25 13:37:02 +0200

    Add ESLint and prettier

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: false,
+  endOfLine: 'lf', // Prevents line issues between windows and osx code
+}

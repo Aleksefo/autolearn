8fab04528823bcce8609333c48ea34095e57cb26
Alex Fomushkin <aleksefo@gmail.com>
2025-03-06 13:11:40 +0100

    Upgrade to Expo 52

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,5 @@
 import { Stack } from 'expo-router'
-import { Head } from 'expo-router/build/head/ExpoHead.android'
-import { Provider, useGlobalState } from '../state/AppContext'
+import { Provider } from '../state/AppContext'
 import { StatusBar } from 'expo-status-bar'
 
 export default function RootLayout() {
